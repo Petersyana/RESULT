ba832dbe99fcd187855f93ee7dcc6b57d35cabb0
Yana <petersyana@rambler.ru>
2022-12-22 19:40:55 +0300

    fixed bugs

--- Program.cs
+++ Program.cs
@@ -3,37 +3,40 @@
 // либо равна 3 символам. Первоначальный массив можно ввести 
 // с клавиатуры, либо задать на старте выполнения алгоритма.
 
-Console.WriteLine("Введите текст. Не меннее 10 слов.");
-string text = string(Console.ReadLine());
+// Console.WriteLine("Введите текст. Не меннее 10 слов.");
+// string text = Console.ReadLine();
+string text = "привет, дом мама ура! солнце 123 забор 2?5 66 нгш ";
 
 string [] separators = {" ", ",", ".", "-", ";", ":", "!", "?"};
-string [] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntrise);
+string [] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
 int i = 0;
 int j = 0;
 string [] newWords = words;
-string s = ' ';
+char s = ' ';
+
+int n = words[0].LastIndexOf(s);
 
 for (i = 0; i < words.Length; i++)
 {
     words[i] = words[i] + s;
-    if ((words[i].LastIndexOf(s) <= 2))
-    {
+   
+    if (((words[i].LastIndexOf(s)-1) <= 2))
          newWords[j] = words[i];
-         Console.Write($" {newWords[j]}");
+    else
+        newWords[j] = new string(" ");
+
          j++;
-    }
 }
 
   void PrintArray(string[] inArray) // вывод на печать
 {
     
-        for (int j = 0; j < inArray.GetLength(1); j++)
+        for (int j = 0; j < inArray.Length; j++)
         {
-            Console.Write($" {inArray[j]} ");
+            Console.Write($" {inArray[j]}");
+            Console.WriteLine();
         }
-        Console.WriteLine();
-    
 }
 
 PrintArray(newWords);
